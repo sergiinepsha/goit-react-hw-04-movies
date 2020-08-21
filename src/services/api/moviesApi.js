2a7bb158c8bd285502394805d57da3aa7410867d
LeanNSP <nsp.lean@gmail.com>
2020-08-21 09:18:37 +0300

    edit moviesApi url

--- src/services/api/moviesApi.js
+++ src/services/api/moviesApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://api.themoviedb.org/3/";
+axios.defaults.baseURL = "https://api.themoviedb.org/3";
 // const AUTH_TOKEN =
 //   "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MzlhYzg2NGE1YzY5NjAxYmI2MjM5MjJmZWYwNmMxMyIsInN1YiI6IjVmMTAwNjQ0NmRlYTNhMDAzMmVlYmQwZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lMdheS0WbtQZdvbSrNacz4F9KqyHQcrKz8A4N828ffo";
 // axios.defaults.headers.common["Authorization"] = AUTH_TOKEN;
@@ -10,7 +10,7 @@ const API_KEY = "439ac864a5c69601bb623922fef06c13";
  * Make a request the weekly trending movies
  */
 const fetchMoviesTrendy = () => {
-  return getAxios(`trending/movie/week?api_key=${API_KEY}`);
+  return getAxios(`/trending/movie/week?api_key=${API_KEY}`);
 };
 
 /*
@@ -18,7 +18,7 @@ const fetchMoviesTrendy = () => {
  */
 const fetchMoviesWithQuery = (searchQuery, page) => {
   return getAxios(
-    `search/movie?api_key=${API_KEY}&query=${searchQuery}&page=${page}&include_adult=false`
+    `/search/movie?api_key=${API_KEY}&query=${searchQuery}&page=${page}&include_adult=false`
   );
 };
 
@@ -26,21 +26,21 @@ const fetchMoviesWithQuery = (searchQuery, page) => {
  * Make a request the primary information about a movie
  */
 const fetchMovieDetails = (movieId) => {
-  return getAxios(`movie/${movieId}?api_key=${API_KEY}`);
+  return getAxios(`/movie/${movieId}?api_key=${API_KEY}`);
 };
 
 /*
  * Make a request the cast and crew movie
  */
 const fetchMovieCast = (movieId) => {
-  return getAxios(`movie/${movieId}/credits?api_key=${API_KEY}`);
+  return getAxios(`/movie/${movieId}/credits?api_key=${API_KEY}`);
 };
 
 /*
  * Make a request the user reviews for a movie
  */
 const fetchMovieReviews = (movieId) => {
-  return getAxios(`movie/${movieId}/reviews?api_key=${API_KEY}`);
+  return getAxios(`/movie/${movieId}/reviews?api_key=${API_KEY}`);
 };
 
 /*
